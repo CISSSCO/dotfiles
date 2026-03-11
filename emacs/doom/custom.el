afa46fdf1d3bf572230e2b1f49f245aa83e28f4a
Cisco Ramon <cisssco581b@gmail.com>
2026-03-11 16:34:39 +0530

    Updated on Mar-11 at 16:34

--- emacs/doom/custom.el
+++ emacs/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-modern zpresent yaml vterm presentation ox-reveal org-superstar org-preview-html impatient-mode exwm epresent doom-themes desktop-environment)))
+   '(undo-tree org-modern zpresent yaml vterm presentation ox-reveal org-superstar org-preview-html impatient-mode exwm epresent doom-themes desktop-environment)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
